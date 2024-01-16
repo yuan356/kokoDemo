d0c5a79af0bee5174996eccc07cc84e0a8bdf2eb
will_chen <willchen356@gmail.com>
2024-01-16 17:34:01 +0800

    Initial Commit

---
+++ WillChenFriendsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WillChenFriendsDemo
+//
+//  Created by Will on 2024/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
